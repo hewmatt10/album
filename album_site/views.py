3d6fccdb67a3b39877652e9e639db81d254b5833
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-04-30 14:41:05 -0400

    home view

--- album_site/views.py
+++ album_site/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-
+from .models import Album, Photo
 # Create your views here.
 
 #display all the albums
 class HomeView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'album_list'
 
+    def get_queryset(self):
+        return Album.objects
 
 
 #create a new album
