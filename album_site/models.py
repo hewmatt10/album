034577e86e4cf4820729fdb313a771a371866926
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-05-01 17:19:52 -0400

    date sorting error

--- album_site/models.py
+++ album_site/models.py
@@ -7,14 +7,14 @@ class Album(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
     size = models.IntegerField(default=0)
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.title
 
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     description = models.CharField(max_length=2000)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.description
 
