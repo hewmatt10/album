68e1ca872c4d068c36e459346098ef75ac96077f
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-05-01 22:20:29 -0400

    fixed forms.py

--- album_site/forms.py
+++ album_site/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from .models import Album
 
 class AlbumForm(forms.Form):
     title = forms.CharField()
     description = forms.CharField()
-    
+
+    def save(self):
+        a = Album.objects.create(title=self.data['title'], description = self.data['description'])
--- album_site/views.py
+++ album_site/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Album, Photo
 from .forms import AlbumForm
@@ -15,12 +15,16 @@ class HomeView(ListView):
         return Album.objects.order_by('-creation_date')
 
 #create a new album
-class NewAlbumView(CreateView):
+class NewAlbumView(FormView):
     model = Album
-    # form_class = AlbumForm
-    fields = ['title', 'description']
+    form_class = AlbumForm
+    # fields = ['title', 'description']
     template_name = 'newalbum.html'
     success_url = '/albums'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 #display a specific album
