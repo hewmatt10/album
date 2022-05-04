43173562261862aa91c5d017fadf3566d9c5bdef
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-05-03 20:36:11 -0400

    no longer failing to show images

--- album_site/urls.py
+++ album_site/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('newalbum/', views.NewAlbumView.as_view(), name='new_album'),
     path('<int:pk>/', views.AlbumView.as_view(), name='current_album'),
 ] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
