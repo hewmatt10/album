cc15e83e1586ffde5ce9890be4195d8701b425df
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-05-29 14:46:45 -0400

    added form, trying to fix the weird whitespace overriding

--- album_site/models.py
+++ album_site/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 class Album(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=2000)
+    description = models.CharField(max_length=500)
     creation_date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.title
 
 class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    description = models.TextField(max_length=2000)
+    description = models.CharField(max_length=500)
     image = models.ImageField(null=True, blank=True)
     date = models.DateTimeField(default=timezone.now)
     def __str__(self):
